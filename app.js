97edba0cf76ad72891bb8aab6139bf4edfda221d
Mark <markjpoole@yahoo.com>
2021-03-14 20:04:42 +0000

    render posts on homepage

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const postSchema = {
  title: String,
@@ -12,8 +12,6 @@ const postSchema = {
 };
 
 const Post = mongoose.model("Post", postSchema);
-
-
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -27,13 +25,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
-
 app.get("/", function(req, res){
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
-    });
+
+  Post.find({}, function(err, posts){
+   res.render("home", {
+     startingContent: homeStartingContent,
+     posts: posts
+     });
+ })
+
 });
 
 app.get("/about", function(req, res){
@@ -52,14 +52,9 @@ app.post("/compose", function(req, res){
   const post = new Post ({
     title: req.body.postTitle,
     content: req.body.postBody
-    post.save()
   });
-  };
-
-  posts.push(post);
-
+  post.save();
   res.redirect("/");
-
 });
 
 app.get("/posts/:postName", function(req, res){
