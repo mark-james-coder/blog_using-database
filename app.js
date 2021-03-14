97643ed5705ada6f523350e9df5725c51eca0978
Mark <markjpoole@yahoo.com>
2021-03-14 20:50:14 +0000

    add callback to save function in app.js

--- app.js
+++ app.js
@@ -53,8 +53,11 @@ app.post("/compose", function(req, res){
     title: req.body.postTitle,
     content: req.body.postBody
   });
-  post.save();
-  res.redirect("/");
+  post.save(function(err){
+   if (!err){
+     res.redirect("/");
+   }
+  });
 });
 
 app.get("/posts/:postName", function(req, res){
